e63f20eddce1c9cd63191c587538079c6c87262a
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-07-04 19:09:30 -0500

    DM2-2628-crear-el-header-servicio-de-mantenimiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
