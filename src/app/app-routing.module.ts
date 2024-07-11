7f61d0b9f766d7a2102db5db107c02d3948b2a13
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-07-10 20:28:04 -0500

    DM2-2628-crear-el-header-servicio-de-mantenimiento
    DM2-2628-crear-el-header-servicio-de-mantenimiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes =
 [
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/home',
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    component: LoginComponent
+    path: 'home',
+    component: DashboardComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { PaymentComponent } from './pages/payment/payment.component';
-import { FooterComponent } from './pages/components/footer/footer.component';
-import { HeaderComponent } from './pages/components/header/header.component';
+
 import { ProductComponent } from './pages/product/product.component';
+import { HeaderComponent } from './pages/components/header/header.component';
+import { FooterComponent } from './pages/components/footer/footer.component';
 
 @NgModule({
   declarations: [
