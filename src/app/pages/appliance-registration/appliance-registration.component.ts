adbe32a96e4d154cc0e57029be3cb57ab1e108e9
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-07-22 09:15:25 -0500

    appliance-registration

---
+++ src/app/pages/appliance-registration/appliance-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appliance-registration',
+  templateUrl: './appliance-registration.component.html',
+  styleUrls: ['./appliance-registration.component.css']
+})
+export class ApplianceRegistrationComponent {
+
+}
