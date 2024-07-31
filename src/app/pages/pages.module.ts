55c62928096591b29e1afef8432d78965edafe69
Fernanda Andrade <119636766+fe222004@users.noreply.github.com>
2024-07-31 08:47:12 -0500

    DM2-2770-pagina-de-pagos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,7 +5,6 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { FormProductModule } from './components/form-product/form-product.module';
 
 import { TranslateModule } from '@ngx-translate/core';
-import { ModalProductComponent } from './components/modal-product/modal-product.component';
 
 @NgModule({
   declarations: [],
